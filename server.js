2f79cc5d3501d82332a974f2c00691b0ca984d29
AZZAMAF <aafazzam@gmail.com>
2025-10-19 12:31:37 +0700

    update server 03

--- server.js
+++ server.js
@@ -1,31 +1,76 @@
+const { log } = require('console');
 const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
+    response.setHeader('Powered-By', 'Node.js');
 
-    const { method } = request;
 
-    if (method === 'GET') {
-        response.end('<h1>Hello!</h1>');
+    const { method, url } = request;
+
+    if (url === '/') {
+    // TODO 2:  logika respons bila url bernilai '/'
+        if (method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }));
+        } else {
+            response.end(JSON.stringify({
+                message: 'Halaman tidak ditemukan!',
+            }));
+        }
+    } else if (url === '/about') {
+
+        // TODO 3: logika respons bila url bernilai '/about'
+        if (method === 'GET') {
+            response.statusCode = 200;
+            // respons bila client menggunakan GET
+            response.end(JSON.stringify({
+                message: 'Halo! Ini adalah halaman about',
+            }));
+
+        } else if (method === 'POST') {
+
+            // respons bila client menggunakan POST
+            if (method === 'POST') {
+                let body = [];
+
+                request.on('data', (chunk) => {
+                    body.push(chunk);
+                });
+
+                request.on('end', () => {
+                    body = Buffer.concat(body).toString();
+                    const { name } = JSON.parse(body);
+                    response.statusCode = 200;
+                    response.end(JSON.stringify({
+                        message: `Halo, ${name}! Ini adalah halaman about`,
+                    }));
+
+                });
+            }
+
+        } else {
+            response.statusCode = 400;
+            // respons bila client tidak menggunakan GET ataupun POST
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses menggunakan ${method}, request`
+            }));
+        }
+    } else {
+        response.statusCode = 404;
+        // TODO 1: logika respons bila url bukan '/' atau '/about'
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan!',
+        }));
     }
 
-    if (method === 'POST') {
-        let body = [];
 
-        request.on('data', (chunk) =>{
-        body.push(chunk);
-    });
 
-    request.on('end', ()=>{
-        body = Buffer.concat(body).toString();
-        const {name} = JSON.parse(body);
-        response.end(`<h1>Hai, ${name}!</h1>`);
 
-    });
-    }
 
-    
+
 };
 
 const server = http.createServer(requestListener);
